e2746f47121d6eccf22d9dd6e5ae894331cd07be
jamesandere <jamesandere56@gmail.com>
2019-03-05 14:47:00 +0300

    Add README file

---
+++ README.md
@@ -0,0 +1,27 @@
+# Pig-dice
+#### This is a a two player die game.
+# Date of current version
+05 March, 2019
+#### By **James Andere**
+## Description
+This is an online game where the players get scores after rolling a dice. Any player who scores 100 points and above wins the game.
+## Setup/Installation Requirements
+* Open your browser.
+* Load github and create an account if you don't have one.
+* Go to my profile (jamesandere).
+* Fork the repository named Pig-dice.
+* Access it on your github account.
+## Known Bugs
+No bugs
+## Technologies Used
+I used HTML for the web structure
+
+I used CSS for styling
+
+I used Javascript for functionality
+## Live Link
+url:
+## Support and contact details
+Contact me through email: jamesandere56@gmail.com
+### License
+Copyright (c) 2019 , **James Andere**
--- js/scripts.js
+++ js/scripts.js
@@ -22,20 +22,18 @@ Roller.prototype.roll = function(){
   else{
     this.yourScoreInThisRound += this.diceRoll;
   }
+
 }
 
 Roller.prototype.hold = function(){
   this.overallScore += this.yourScoreInThisRound;
   this.yourScoreInThisRound = 0;
-  alert("Next player");
-}
-
-Roller.prototype.winner = function(){
-if(this.overallScore >= 10)  {
-  alert("You won");
-}
-freshGame();
-}
+  document.getElementById("nex").innerHTML="Your turn is over, next player.";
+  if(this.overallScore >99) {
+    document.getElementById("win").innerHTML="You won";
+  }
+ }
+ 
 
 Roller.prototype.freshGame = function(){
   this.diceRoll=0;
@@ -43,19 +41,11 @@ Roller.prototype.freshGame = function(){
   this.overallScore=0;
 }
 
-
-
-
-//UI logic
-$(document).ready(function(){
-$("#freshGame").click(function() {
-  $("#game").empty();
-  $("#round").empty();
-  $("#ov").empty();
-})
-playerOne = new Roller();
-playerTwo = new Roller();
-$("#one").click(function(){
+ //UI logic
+ $(document).ready(function(){
+ playerOne = new Roller();
+ playerTwo = new Roller();
+ $("#one").click(function(){
   playerOne.diceRoll = rollingDice();
   $("#game").text(playerOne.diceRoll);
   playerOne.roll();
@@ -74,6 +64,8 @@ $("#hold1").click(function(){
   $("#ov").text(playerOne.overallScore);
   $("#game").empty();
   $("#round").empty();
+  $(".pl1").hide();
+  $(".pl2").show();
 })
 
 $("#hold2").click(function(){
@@ -81,131 +73,49 @@ $("#hold2").click(function(){
   $("#ov2").text(playerTwo.overallScore);
   $("#gam").empty();
   $("#round2").empty();
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function(){
-//   $("#start").click(function(){
-//     $(".head").slideUp();
-//     $(".rules").show();
-//   });
-//
-//   $("#rule").click(function(){
-//     $(".rules").hide();
-//     $(".dets").show();
-//   });
-//
-//   $("#next").click(function(){
-//     $(".dets").hide();
-//     $(".wel").show();
-//
-//     var name = $("#name").val();
-//     var email = $ ("#email").val();
-//     playerOne = new Player(name,email);
-//     $(".name").text(playerOne.name);
-//     $(".em").text(playerOne.email);
-//
-//     var name2 = $("#name2").val();
-//     var email2 = $ ("#email2").val();
-//     playerTwo = new Player(name2,email2);
-//     $(".name2").text(playerTwo.name);
-//     $(".em2").text(playerTwo.email);
-//   });
-//
-//   $("#proc").click(function(){
-//     $(".wel").hide();
-//     $(".play").show();
-//   });
-//
-//   $("#new").click(function(){
-//     $(".play").hide();
-//     $(".head").show();
-//   })
-//
-//   $("#hold1").click(function(){
-//     playerOne.hold();
-//   })
-// });
+  $(".pl2").hide();
+  $(".pl1").show();
+  $("#nex").hide();
+ })
+ })
+
+$(document).ready(function(){
+  $("#start").click(function(){
+    $(".head").slideUp();
+    $(".rules").show();
+  });
+
+  $("#rule").click(function(){
+    $(".rules").hide();
+    $(".dets").show();
+  });
+
+  $("#next").click(function(){
+    $(".dets").hide();
+    $(".wel").show();
+    $("#freshGame").show();
+
+    var name = $("#name").val();
+    var email = $("#email").val();
+    playerOne = new Player(name,email);
+    $(".name").text(playerOne.name);
+    $(".em").text(playerOne.email);
+
+    var name2 = $("#name2").val();
+    var email2 = $("#email2").val();
+    playerTwo = new Player(name2,email2);
+    $(".name2").text(playerTwo.name2);
+    $(".em2").text(playerTwo.email2);
+  });
+
+  $("#proc").click(function(){
+    $(".wel").hide();
+    $(".play").show();
+  });
+
+  $("#freshGame").click(function(){
+    $(".play").hide();
+    $(".head").show();
+  })
+
+});
